5f5bbdf49a190a4a91eda314ec4c82f1cb95db87
marcialcarrillo <marcialcarrillov@gmail.com>
2022-07-11 11:36:25 -0600

    fix: heroku mem error

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -1,12 +1,6 @@
 const handleDBDate = (DBDate) => {
   const date = new Date(DBDate);
-
-  // return date.toLocaleString("en-GB", {
-  //   day: "numeric",
-  //   month: "numeric",
-  //   year: "numeric",
-  // });
-
+  
   return date.toLocaleString();
 };
 
